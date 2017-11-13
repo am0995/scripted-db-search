c25f53ebf45b8cb4eceb6a47099afbbedc12489c
am0995 <alyssam0995@tywls.org>
2017-11-13 22:10:18 +0000

    Working on the typeof function

--- db-search-functions.js
+++ db-search-functions.js
@@ -10,33 +10,69 @@ function getVersion(handleVersion) {
     // the database snapshot and passes it as an argument to the 'handleVersion'
     // callback.
     
-    //var version = snapshot.val();
-    //handleVersion(version);
+    var version = snapshot.val();
+    handleVersion(version);
   });
 }
 
 function getHelloWorldValue(handleType) {
-  // TODO: Your code goes here. Hint: use 'getVersion' as a template.
+    var database = firebase.database();
+  var databaseVersionReference = database.ref('/db-search/hello-world');
+  databaseVersionReference.once('value').then(function(snapshot) {
+    
+    var version = snapshot.val();
+    handleType(version);
+  });
 }
 
 function applyDiscount(handleNewPrice) {
-  // TODO: Your code goes here.
+    var database = firebase.database();
+  var databaseVersionReference = database.ref('/db-search/discount');
+  databaseVersionReference.once('value').then(function(snapshot) {
+   
+    var version = snapshot.val();
+    handleNewPrice(version * 100);
+  });
 }
 
 function getArrayLength(handleLength) {
-  // TODO: Your code goes here.
+    var database = firebase.database();
+  var databaseVersionReference = database.ref('/db-search/array');
+  databaseVersionReference.once('value').then(function(snapshot) {
+    
+    var version = snapshot.val();
+    handleLength(version.length);
+  });
 }
 
 function getLastArrayElement(handleElement) {
-  // TODO: Your code goes here.
+   var database = firebase.database();
+  var databaseVersionReference = database.ref('/db-search/array');
+  databaseVersionReference.once('value').then(function(snapshot) {
+    
+    var version = snapshot.val();
+    handleElement(version[6]);
+  });
 }
 
 function getArrayJoin(handleJoin) {
-  // TODO: Your code goes here.
+    var database = firebase.database();
+  var databaseVersionReference = database.ref('/db-search/array');
+  databaseVersionReference.once('value').then(function(snapshot) {
+    
+    var version = snapshot.val();
+    handleJoin(version.join(" "));
+  });
 }
 
 function getNumberOrStringType(handleType) {
-  // TODO: Your code goes here.
+    var database = firebase.database();
+  var databaseVersionReference = database.ref('/db-search/number-or-string');
+  databaseVersionReference.once('value').then(function(snapshot) {
+    
+    var version = snapshot.val();
+    handleType(version);
+  });
 }
 
 function getObjectValue(handleValue) {
